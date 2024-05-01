464610fd81259f2d0e37580e49c7b2b5c667b8de
anirudhasarma30 <anirudhasarma30@gmail.com>
2024-05-01 00:06:30 -0400

    created new env and user.py file

---
+++ model/user.py
@@ -0,0 +1,10 @@
+
+
+class USER:
+    def __init__(self,
+                 name,
+                 email,
+                 password,
+                 creation_timestamp,
+                 ) -> None:
+        pass
